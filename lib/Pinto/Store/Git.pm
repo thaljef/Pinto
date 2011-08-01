5091832d6c4eb5d4c353479310e5d29a8c43bb7e
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-08-01 11:26:46 -0700

    Added placeholder for the Git store.

---
+++ lib/Pinto/Store/Git.pm
@@ -0,0 +1,23 @@
+package Pinto::Store::Git;
+
+# ABSTRACT: Store your Pinto repository with Git
+
+use Moose;
+
+extends 'Pinto::Store';
+
+#-------------------------------------------------------------------------------
+
+# VERSION
+
+#-------------------------------------------------------------------------------
+
+1;
+
+__END__
+
+=head1 DESCRIPTION
+
+This module is Not yet implimented.
+
+=cut
