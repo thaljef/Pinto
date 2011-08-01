71c30ba89ed9d8107bdb14d5dd58ef16da22fa87
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-08-01 14:12:48 -0700

    Go back to my Moose-ish version of Package.pm

--- lib/Pinto/Package.pm
+++ lib/Pinto/Package.pm
@@ -2,8 +2,7 @@ package Pinto::Package;
 
 # ABSTRACT: Represents a single record in the 02packages.details.txt file
 
-use strict;
-use warnings;
+use Moose;
 
 use Path::Class qw();
 
@@ -13,30 +12,99 @@ use Path::Class qw();
 
 #------------------------------------------------------------------------------
 
-sub new {
-    my ($class, %args) = @_;
-    $args{version} ||= 'undef';
-    $args{author} ||=  Path::Class::file( $args{file} )->dir()->dir_list(2, 1);
-    return bless \%args, $class;
+=attr name()
+
+Returns the name of this Package as a string.  For example, C<Foo::Bar>.
+
+=cut
+
+has 'name'   => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+=attr version()
+
+Returns the version of this Package as a string.  This could be a number
+or some "version string", such as C<1.5.23>.
+
+=cut
+
+has 'version' => (
+    is        => 'ro',
+    isa       => 'Str',
+    required  => 1,
+);
+
+=attr file()
+
+Returns the path to the file this Package lives in, as a string.  The path
+is as it appears in the C<02packages.details.txt> file.  So it will be
+in Unix format and relative to the F<authors/id> directory.
+
+=cut
+
+has 'file'    => (
+    is        => 'ro',
+    isa       => 'Str',
+    required  => 1,
+);
+
+=attr native_file()
+
+Same as the C<file()> method, but returns the path as a
+L<Path::Class::File> object that is suitable for your OS.
+
+has 'native_file' => (
+    is            => 'ro',
+    isa           => 'Path::Class::File',
+    lazy          => 1,
+    init_arg      => undef,
+    default       => sub { Path::Class::File->new( $_[0]->file() ) },
+);
+
+
+=attr author()
+
+Returns the author of this Package.  The author is extracted from the
+path to the file this Package lives in.  For example, the author of
+F<J/JO/JOHN/Foo-Bar-1.2.tar.gz> will be C<JOHN>.
+
+=cut
+
+has 'author'  => (
+    is        => 'ro',
+    isa       => 'Str',
+    lazy      => 1,
+    init_arg  => undef,
+    builder   => '__build_author',
+);
+
+#------------------------------------------------------------------------------
+
+# TODO: Declare subtype for the 'file' attribute and coerce it from a
+# Path::Class::File to a string that always looks like a Unix path.
+
+#------------------------------------------------------------------------------
+
+sub __build_author {
+    my ($self) = @_;
+    return Path::Class::file( $self->file() )->dir()->dir_list(2, 1);
 }
 
-#------------------------------------------------------------------------------
-# Accessors
+=method to_string()
 
-sub name        { return $_[0]->{name} }
-sub file        { return $_[0]->{file} }
-sub version     { return $_[0]->{version} }
-sub author      { return $_[0]->{author} }
-sub native_file { return Path::Class::file( $_[0]->{file} ) }
+Returns this Package object in a format that is suitable for writing
+to an F<02packages.details.txt> file.
 
-#------------------------------------------------------------------------------
-# Methods
+=cut
 
 sub to_string {
     my ($self) = @_;
     my $fw = 38 - length $self->version();
     $fw = length $self->name() if $fw < length $self->name();
-    return sprintf "%-${fw}s %s  %s", $self->{name}, $self->{version}, $self->{file};
+    return sprintf "%-${fw}s %s  %s", $self->name(), $self->version(), $self->file();
 }
 
 #------------------------------------------------------------------------------
