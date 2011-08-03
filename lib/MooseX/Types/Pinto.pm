a7f244f6742e0f1ee5fe488f47889b324498863e
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-08-03 11:44:27 -0700

    Added our own type library.

---
+++ lib/MooseX/Types/Pinto.pm
@@ -0,0 +1,60 @@
+package MooseX::Types::Pinto;
+
+# ABSTRACT: Moose types used within Pinto
+
+use strict;
+use warnings;
+
+use MooseX::Types -declare => [ qw( AuthorID URI Dir File) ];
+use MooseX::Types::Moose qw( Str );
+
+use URI;
+use Path::Class::Dir;
+use Path::Class::File;
+
+use namespace::autoclean;
+
+#-----------------------------------------------------------------------------
+
+# VERSION
+
+#-----------------------------------------------------------------------------
+
+subtype AuthorID,
+    as Str,
+    where { $_ !~ /\W/ },
+    message { "The author ($_) can only be alphanumeric characters" };
+
+coerce AuthorID,
+    from Str,
+    via  { uc $_ };
+
+#-----------------------------------------------------------------------------
+
+class_type URI, {class => 'URI'};
+
+coerce URI,
+    from Str,
+    via { 'URI'->new($_) };
+
+#-----------------------------------------------------------------------------
+
+subtype Dir, as 'Path::Class::Dir';
+
+coerce Dir,
+    from Str,
+    via { Path::Class::Dir->new($_) };
+
+#-----------------------------------------------------------------------------
+
+subtype File, as 'Path::Class::File';
+
+coerce File,
+    from Str,
+    via { Path::Class::File->new($_) };
+
+#-----------------------------------------------------------------------------
+
+1;
+
+__END__
