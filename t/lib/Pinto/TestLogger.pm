5760739331cffd1359441084be7ccee6ae8c96ca
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-08-12 14:47:45 -0700

    Created some classes to help with testing.
    These aren't really mock objects.  Rather,
    they are just subclasses of the regular ones
    but their side-effecting behaviors have been
    neutralized.

---
+++ t/lib/Pinto/TestLogger.pm
@@ -0,0 +1,26 @@
+package Pinto::TestLogger;
+
+# ABSTRACT: A logger for testing Pinto
+
+use Moose;
+
+extends 'Pinto::Logger';
+
+#-----------------------------------------------------------------------------
+
+has '+config' => (
+    required => 0,
+);
+
+override _build_log_level => sub { return 1 };
+
+#-----------------------------------------------------------------------------
+
+=head1 DESCRIPTION
+
+L<Pinto::TestLogger> is a subclass of L<Pinto::Logger> than is used
+for unit testing the rest of L<Pinto>.  L<Pinto::TestLogger> does not
+require a L<Pinto::Config> attribute and the C<log_level> is always 1
+(i.e. debug).
+
+=cut
