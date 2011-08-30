273f7c6cb244c110c379e3c2aad79738cc4a2d60
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-08-30 11:45:19 -0700

    Fix bug due to rename.

--- lib/App/Pinto/Admin/Command/update.pm
+++ lib/App/Pinto/Admin/Command/update.pm
@@ -47,7 +47,7 @@ sub execute {
     my ($self, $opts, $args) = @_;
 
     $self->pinto->new_action_batch( %{$opts} );
-    $self->pinto->add_action('Mirror', %{$opts});
+    $self->pinto->add_action('Update', %{$opts});
     my $result = $self->pinto->run_actions();
     return $result->is_success() ? 0 : 1;
 }
