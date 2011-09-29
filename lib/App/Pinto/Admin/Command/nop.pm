de087014fedc97383346d31338f563878ad819f9
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-09-29 15:14:58 -0700

    Check that sleep argument is positive.

--- lib/App/Pinto/Admin/Command/nop.pm
+++ lib/App/Pinto/Admin/Command/nop.pm
@@ -31,7 +31,11 @@ sub opt_spec {
 sub validate_args {
     my ($self, $opts, $args) = @_;
 
-    $self->usage_error('Arguments are not allowed') if @{ $args };
+    $self->usage_error('Arguments are not allowed')
+      if @{ $args };
+
+    $self->usage_error('Sleep time must be positive integer')
+      if defined $opts->{sleep} && $opts->{sleep} < 1;
 
     return 1;
 }
