43a3c4181fb210dc521518d31023274c1ee5d352
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-11-30 15:47:50 -0800

    Pinto::Repository is now dumb.
    It only knows how to do 2 things: add a dist, and remove a dist.
    When I say "add" and "remove", I mean that it just tells the store
    to add a file and puts some data in the database.
    
    How that file gets there and what data is put into the database
    is up to the Action.  This makes sense, because creating an Action
    class is the primary means of extending Pinto.  So it has to
    do most of the work of fetching a dist file, extracting its
    packages, etc.

--- lib/Pinto/Repository.pm
+++ lib/Pinto/Repository.pm
@@ -5,13 +5,10 @@ package Pinto::Repository;
 use Moose;
 
 use Class::Load;
-use Path::Class;
 
 use Pinto::Database;
 use Pinto::IndexCache;
-use Pinto::PackageExtractor;
-use Pinto::DistributionSpec;
-use Pinto::Exceptions qw(throw_fatal throw_error);
+use Pinto::Exceptions qw(throw_fatal);
 use Pinto::Types qw(Dir);
 
 use namespace::autoclean;
@@ -26,7 +23,7 @@ use namespace::autoclean;
 has db => (
     is         => 'ro',
     isa        => 'Pinto::Database',
-    handles    => [ qw(write_index) ],
+    handles    => [ qw(write_index select_distributions select_packages) ],
     lazy_build => 1,
 );
 
@@ -46,13 +43,6 @@ has cache => (
 );
 
 
-has package_extractor => (
-    is         => 'ro',
-    isa        => 'Pinto::PackageExtractor',
-    lazy_build => 1,
-);
-
-
 has root_dir => (
     is         => 'ro',
     isa        => Dir,
@@ -65,8 +55,7 @@ has root_dir => (
 # Roles
 
 with qw( Pinto::Interface::Configurable
-         Pinto::Interface::Loggable
-         Pinto::Role::FileFetcher );
+         Pinto::Interface::Loggable );
 
 #-------------------------------------------------------------------------------
 # Builders
@@ -102,128 +91,28 @@ sub _build_cache {
 }
 
 #-------------------------------------------------------------------------------
-
-sub _build_package_extractor {
-    my ($self) = @_;
-
-    return Pinto::PackageExtractor->new( config => $self->config(),
-                                         logger => $self->logger() );
-}
-
-#-------------------------------------------------------------------------------
 # Methods
 
 sub add_distribution {
-    my ($self, %args) = @_;
+    my ($self, $struct) = @_;
 
-    my $archive = $args{archive};
-    my $author  = $args{author};
+    my $dist = $self->db->new_distribution($struct);
 
-    throw_error "Archive $archive does not exist"  if not -e $archive;
-    throw_error "Archive $archive is not readable" if not -r $archive;
+    $self->db->insert_distribution($dist);
 
-    my $root_dir   = $self->config->root_dir();
-    my $basename   = $archive->basename();
-    my $author_dir = Pinto::Util::author_dir($author);
-    my $path       = $author_dir->file($basename)->as_foreign('Unix')->stringify();
-
-    my $where    = {path => $path};
-    my $existing = $self->db->select_distributions( $where )->single();
-    throw_error "Distribution $path already exists" if $existing;
-
-    my @package_specs = $self->package_extractor->provides(archive => $archive);
-    $self->whine("$archive contains no packages") if not @package_specs;
-
-    for my $pkg (@package_specs) {
-        my $attrs = { prefetch => 'distribution' };
-        my $where = { name => $pkg->{name}, 'distribution.source' => 'LOCAL'};
-        my $incumbent = $self->db->select_packages($where, $attrs)->first() or next;
-        if ( (my $incumbent_author = $incumbent->author()) ne $author ) {
-            throw_error "Only author $incumbent_author can update package $pkg->{name}";
-        }
-    }
-
-    my $count = @package_specs;
-    $self->info("Adding distribution $path providing $count packages");
-
-    my $dist_spec = Pinto::DistributionSpec->new( path     => $path,
-                                                  source   => 'LOCAL',
-                                                  packages => \@package_specs );
-
-    my $dist = $self->db->insert_distribution($dist_spec);
-    $self->store->add_archive( $archive => $dist->archive($root_dir) );
+    $self->store->add_archive( $dist->archive( $self->root_dir() ) );
 
     return $dist;
-}
-
-#-------------------------------------------------------------------------------
-
-sub import_distribution {
-    my ($self, %args) = @_;
-
-    my $url  = $args{url};
-    my ($source, $path, $author, $archive) =
-      Pinto::Util::parse_dist_url( $url, $self->config->root_dir() );
-
-    my $where    = {path => $path};
-    my $existing = $self->db->select_distributions( $where )->single();
-    throw_error "Distribution $path already exists" if $existing;
-
-    $self->fetch( url => $url, to => $archive );
-
-    my @package_specs = $self->package_extractor->provides(archive => $archive);
-    $self->whine("$archive contains no packages") if not @package_specs;
-
-    my $count = @package_specs;
-    $self->info("Importing distribution $url providing $count packages");
-
-    my $dist_spec = Pinto::DistributionSpec->new( path     => $path,
-                                                  source   => $source,
-                                                  packages => \@package_specs );
-
-    my $dist = $self->db->insert_distribution( $dist_spec );
-    $self->store->add_archive( $archive );
-
-    return $dist;
-}
-
-#-------------------------------------------------------------------------------
-
-sub mirror_distribution {
-    my ($self, %args) = @_;
-
-    my $url = $args{url};
-
-    my @path_parts = split m{ / }mx, $url;
-    my $destination = $self->repos->root_dir->file( qw(authors id), @path_parts );
-
-    $self->fetch(url => $url, to => $destination);
-
-    #my $dist = $self->repos->db->new_distribution( %{ $dist_spec } );
-    #$self->repos->db->insert_distribution($dist, @{ $dist_spec->{packages} });
-    #$self->repos->store->add_archive($destination);
-
-    return 1;
 }
 
 #-------------------------------------------------------------------------------
 
 sub remove_distribution {
-    my ($self, %args) = @_;
-
-    my $path = $args{path};
-
-    my $where = {path => $path};
-    my $dist  = $self->db->select_distributions( $where )->single()
-        or throw_error "Distribution $path does not exist";
-
-    my $count = $dist->package_count();
-    $self->info("Removing distribution $dist with $count packages");
+    my ($self, $dist) = @_;
 
     $self->db->delete_distribution($dist);
 
-    my $archive = $dist->archive( $self->config->root_dir() );
-    $self->store->remove_archive($archive);
+    $self->store->remove_archive( $dist->archive( $self->root_dir() ) );
 
     return $dist;
 }
