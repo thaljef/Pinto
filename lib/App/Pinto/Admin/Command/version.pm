af32581c7325bda6f7cfca351e0ac4ffe97d5917
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-12-04 02:33:52 -0800

    List App version before Pinto version.

--- lib/App/Pinto/Admin/Command/version.pm
+++ lib/App/Pinto/Admin/Command/version.pm
@@ -28,14 +28,14 @@ sub usage_desc {
 sub execute {
     my ($self, $opts, $args) = @_;
 
+    my $app_class = ref $self->app();
+    my $app_version = $self->app->VERSION();
+    printf "$app_class $app_version\n";
+
     my $pinto_class = $self->app->pinto_class();
     Class::Load::load_class( $pinto_class );
     my $pinto_version = $pinto_class->VERSION();
     printf "$pinto_class $pinto_version\n";
-
-    my $app_class = ref $self->app();
-    my $app_version = $self->app->VERSION();
-    printf "$app_class $app_version\n";
 
     return 0;
 }
