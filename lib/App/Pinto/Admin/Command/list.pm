92d1c0eccb3dd1ded31becb2208a68b79166b368
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-12-07 15:10:37 -0800

    POD edits.

--- lib/App/Pinto/Admin/Command/list.pm
+++ lib/App/Pinto/Admin/Command/list.pm
@@ -75,31 +75,40 @@ None.
 
 =item --format=FORMAT_SPECIFICATION
 
-Specifies how the output should be formatted using C<printf>-like
-placeholders.  The following placeholders are allowed:
+Sets the format of the output using C<printf>-style placeholders.
+Valid placeholders are:
 
   Placeholder    Meaning
-  -----------------------------------------------------------------------
-  n              Package name
-  N              Package name-version
-  v              Package version
-  V              Package numeric version
-  m              Package maturity:      [D] = developer  [R] = release
-  x              Index status:          [*] = latest     [-] = ineligible
-  p              Logical distribution path
-  P              Native distribtuion path (relative to the repository)
-  s              Distribution source:   [L] = local      [F] = foreign
-  S              Distribution source URL
-  d              Distribution name
-  D              Distribution name-version
-  w              Distribution version
-  W              Distribution numeric version
-  u              Distribution url
-  -----------------------------------------------------------------------
+  -----------------------------------------------------------------------------
+  %n             Package name
+  %N             Package name-version
+  %v             Package version
+  %x             Index status:                   (@) = is latest
+  %y             Pin status:                     (+) = is pinned
+  %m             Distribution maturity:          (d) = developer, (r) = release
+  %p             Distribution index path [1]
+  %P             Distribution physical path [2]
+  %s             Distribution origin:            (l) = local, (f) = foreign
+  %S             Distribution source repository
+  %a             Distribution author
+  %d             Distribution name
+  %D             Distribution name-version
+  %w             Distribution version
+  %u             Distribution url
+  %%             A literal '%'
 
-The default format is: C<%x%m%s %n %v %p\n>.  See L<String::Format>
-for additional information on the formatting capabilities, such as
-specifying field width, alignment, and padding.
+
+  [1]: The index path is always a Unix-style path fragment, as it
+       appears in the 02packages.details.txt index file.
+
+  [2]: The physical path is always in the native style for this OS,
+       and is relative to the root directory of the repository.
+
+You can also specify the minimum field widths and left or right
+justification, using the usual notation.  For example, this is what
+the default format looks like.
+
+  %x%m%s %-38n %v %p\n
 
 =item --noinit
 
