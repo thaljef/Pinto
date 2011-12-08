4f98332b2649df9447a9ae24f57ccb877b59bd26
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-12-08 10:26:11 -0800

    Add a helpful log message.

--- lib/Pinto/Action/Purge.pm
+++ lib/Pinto/Action/Purge.pm
@@ -21,6 +21,9 @@ override execute => sub {
 
     my $dists = $self->repos->db->select_distributions();
 
+    my $count = $dists->count();
+    $self->info("Removing all $count distributions from the repository");
+
     my $removed = 0;
     while ( my $dist = $dists->next() ) {
         $self->repos->remove_distribution($dist);
