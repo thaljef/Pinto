74f89b59d592d8153de338c59b7e062a35f8ca8f
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-12-10 02:39:39 -0800

    First attempt at Git store.

--- lib/Pinto/Store/VCS/Git.pm
+++ lib/Pinto/Store/VCS/Git.pm
@@ -4,47 +4,106 @@ package Pinto::Store::VCS::Git;
 
 use Moose;
 
-use Carp;
-
-extends 'Pinto::Store::VCS';
+use Git::Repository;
 
 #-------------------------------------------------------------------------------
 
 # VERSION
 
 #-------------------------------------------------------------------------------
+# ISA
+
+extends 'Pinto::Store::VCS';
+
+#-------------------------------------------------------------------------------
+
+has _git => (
+    is       => 'ro',
+    isa      => 'Git::Repository',
+    default  => sub { Git::Repository->new( work_tree => $_[0]->config->root_dir() ) },
+    init_arg => undef,
+    lazy     => 1,
+);
+
+#-------------------------------------------------------------------------------
 
 augment initialize => sub {
-    croak __PACKAGE__ . 'is not implemented yet';
-    # git clone or git pull
+    my ($self) = @_;
+
+    my $root_dir = $self->config->root_dir();
+    $self->note('Updating working copy');
+    $self->_git->run( qw(pull) );
+
+    return $self;
 };
 
 #-------------------------------------------------------------------------------
 
 augment commit => sub {
-    croak __PACKAGE__ . 'is not implemented yet';
-    # git commit and push
+    my ($self, %args) = @_;
+
+    my $message = $args{message} || 'NO MESSAGE WAS GIVEN';
+
+    # There could be a lot of paths.  Some OS have a limit on the
+    # number of arguments a command can have.  To workaround this, we
+    # pass the paths over STDIN (via that 'input' paramter).
+
+    my $paths   = join "\n", map { $_->stringify() } @{ $self->paths_to_commit() };
+
+    $self->_git->run( 'commit' => '-m', $message, {input => $paths} );
+
+    $self->_git->run( qw(push -u origin master) );
+
+    return $self;
 };
 
 #-------------------------------------------------------------------------------
 
 augment tag => sub {
-    croak __PACKAGE__ . 'is not implemented yet';
-    # git tag
+    my ($self, $tag) = @_;
+
+    my $message = 'Tagging repository as of...';
+
+    $self->_git->run( 'tag' => '-m', $message, $tag );
+
+    return $self;
 };
 
 #-------------------------------------------------------------------------------
 
 augment add_path => sub {
-    croak __PACKAGE__ . 'is not implemented yet';
-    # git add
+    my ($self, %args) = @_;
+
+
+    my $path = $args{path};
+    $self->debug("Scheduling $path for addition to VCS");
+
+    # With git, all paths must be relative to the top of the work tree
+    $path = $path->relative( $self->config->root_dir() );
+
+    $self->_git->run( 'add' => $path->stringify() );
+
+    $self->mark_path_for_commit($path);
+
+    return $self;
 };
 
 #-------------------------------------------------------------------------------
 
 augment remove_path => sub {
-    croak __PACKAGE__ . 'is not implemented yet';
-    # git rm
+    my ($self, %args) = @_;
+
+    my $path  = $args{path};
+    $self->debug("Scheduling $path for removal from VCS");
+
+    # With git, all paths must be relative to the top of the work tree
+    $path = $path->relative( $self->config->root_dir() );
+
+    $self->_git->run( 'rm' => '-f',  $path->stringify() );
+
+    $self->mark_path_for_commit($path);
+
+    return $self;
 };
 
 #-------------------------------------------------------------------------------
