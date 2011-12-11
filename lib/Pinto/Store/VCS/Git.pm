685bc56fdbaeef0bde58bd5ee509aec4ff7183cf
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-12-11 00:33:22 -0800

    Document Git Stores.

--- lib/Pinto/Store/VCS/Git.pm
+++ lib/Pinto/Store/VCS/Git.pm
@@ -6,6 +6,8 @@ use Moose;
 
 use Git::Repository;
 
+use namespace::autoclean;
+
 #-------------------------------------------------------------------------------
 
 # VERSION
@@ -18,14 +20,25 @@ extends 'Pinto::Store::VCS';
 #-------------------------------------------------------------------------------
 
 has _git => (
-    is       => 'ro',
-    isa      => 'Git::Repository',
-    default  => sub { Git::Repository->new( work_tree => $_[0]->config->root_dir() ) },
-    init_arg => undef,
-    lazy     => 1,
+    is         => 'ro',
+    isa        => 'Git::Repository',
+    init_arg   => undef,
+    lazy_build => 1,
 );
 
 #-------------------------------------------------------------------------------
+# Builders
+
+sub _build__git {
+    my ($self) = @_;
+
+    my $root_dir = $self->config->root_dir();
+
+    return Git::Repository->new( work_tree => $root_dir );
+}
+
+#-------------------------------------------------------------------------------
+# Methods
 
 augment add_path => sub {
     my ($self, %args) = @_;
@@ -94,12 +107,55 @@ augment tag => sub {
 
 #-------------------------------------------------------------------------------
 
+__PACKAGE__->meta->make_immutable();
+
+#-------------------------------------------------------------------------------
+
 1;
 
 __END__
 
+=head1 SYNOPSIS
+
+  # If you don't already have a Pinto repository, create one (notice the --store option here)
+  $> pinto-admin --repos=~/PINTO create --store=Pinto::Store::VCS::Git
+
+  # If you do already have a repository, edit its config (at $REPOS/.pinto/config/pinto.ini)
+  store = Pinto::Store::VCS::Git
+
+  # Initialize Pinto repository into Git
+  $> cd ~/PINTO
+  $> git init
+
+  # Add initial files and commit
+  $> git add .pinto authors modules
+  $> git commit -a -m 'New Pinto repos'
+
+Now run L<pinto-admin> or L<pinto-server> as you normally would,
+setting the C<--repos> to the path of the working copy (which would be
+F<~/PINTO> in the example above).
+
 =head1 DESCRIPTION
 
-This module is Not yet implemented.
+L<Pinto::Store::VCS::Git> is a back-end for L<Pinto> that stores the
+repository inside a local Git repository.  Before you can effectively
+use this Store, you must initialize or clone a Git repository that
+contains your Pinto repository (see L</"SYNOPSIS"> for the typical
+procedure).
+
+Note this Store only works with a local Git repository (i.e. one that
+does not push or pull to another repository).  If you want to do that,
+see L<Pinto::Store::VCS::Git::Remote).
+
+=head1 CAVEATS
+
+=over 4
+
+=item The C<git> program is required.
+
+You must have the binary C<git> tool installed somwhere in uour
+C<$PATH> for this Store to work.
+
+=back
 
 =cut
