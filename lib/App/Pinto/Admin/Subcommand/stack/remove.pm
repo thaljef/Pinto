7d9c807a65d876e90a518e4413f8a34f90cf74e2
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-03-05 23:22:58 -0800

    Filled out the Stack::Remove action and subcommand.

--- lib/App/Pinto/Admin/Subcommand/stack/remove.pm
+++ lib/App/Pinto/Admin/Subcommand/stack/remove.pm
@@ -1,11 +1,9 @@
 package App::Pinto::Admin::Subcommand::stack::remove;
 
-# ABSTRACT: remove a stack
+# ABSTRACT: delete a stack
 
 use strict;
 use warnings;
-
-use Pinto::Util;
 
 #-----------------------------------------------------------------------------
 
@@ -21,10 +19,54 @@ sub command_names { qw(remove rm delete del) }
 
 #------------------------------------------------------------------------------
 
+sub opt_spec {
+    my ($self, $app) = @_;
+
+    return (
+        [ 'message|m=s'     => 'Prepend a message to the VCS log'  ],
+        [ 'nocommit'        => 'Do not commit changes to VCS'      ],
+        [ 'noinit'          => 'Do not pull/update from VCS'       ],
+        [ 'tag=s'           => 'Specify a VCS tag name'            ],
+    );
+
+
+}
+
+#------------------------------------------------------------------------------
+sub validate_args {
+    my ($self, $opts, $args) = @_;
+
+    $self->usage_error('Must specify a stack name')
+        if not @{$args};
+
+    return 1;
+}
+
+#------------------------------------------------------------------------------
+
+sub usage_desc {
+    my ($self) = @_;
+
+    my ($command) = $self->command_names();
+
+    my $usage =  <<"END_USAGE";
+%c --root=PATH stack $command [OPTIONS] STACK_NAME
+END_USAGE
+
+    chomp $usage;
+    return $usage;
+}
+
+#------------------------------------------------------------------------------
+
 sub execute {
     my ($self, $opts, $args) = @_;
 
-    print "REMOVING STACK!\n";
+    $self->pinto->new_batch(%{$opts});
+    $self->pinto->add_action($self->action_name(), %{$opts}, stack => $args->[0]);
+    my $result = $self->pinto->run_actions();
+
+    return $result->is_success() ? 0 : 1;
 }
 
 #------------------------------------------------------------------------------
---
+++ lib/Pinto/Action/Stack/Remove.pm
@@ -0,0 +1,63 @@
+package Pinto::Action::Stack::Remove;
+
+# ABSTRACT: An action to delete a stack
+
+use Moose;
+
+use MooseX::Types::Moose qw(Str);
+use Pinto::Types qw(StackName);
+
+use Pinto::Exceptions qw(throw_fatal);
+
+use namespace::autoclean;
+
+#------------------------------------------------------------------------------
+
+# VERSION
+
+#------------------------------------------------------------------------------
+# ISA
+
+extends 'Pinto::Action';
+
+#------------------------------------------------------------------------------
+# Attributes
+
+has stack => (
+    is       => 'ro',
+    isa      => StackName,
+    required => 1,
+    coerce   => 1,
+);
+
+#------------------------------------------------------------------------------
+# Methods
+
+override execute => sub {
+    my ($self) = @_;
+
+    my $stack_name = $self->stack();
+
+    $self->fatal( 'You cannot remove the default stack' )
+      if $stack_name eq 'default';
+
+    my $where = {name => $stack_name};
+    my $stack = $self->repos->db->select_stacks( $where )->single()
+        or $self->fatal("Stack $stack_name does not exist");
+
+    $self->note("Removing stack $stack");
+
+    $stack->delete();
+
+    return 1;
+};
+
+#------------------------------------------------------------------------------
+
+__PACKAGE__->meta->make_immutable();
+
+#------------------------------------------------------------------------------
+
+1;
+
+__END__
