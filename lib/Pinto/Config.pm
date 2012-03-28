cc8fb83f7240f45190af045fa8962f2fcd619cbf
Karen Etheridge <ether@cpan.org>
2012-03-28 21:55:25 +0000

    make root_dir an alias, rather than its own attribute

--- lib/Pinto/Config.pm
+++ lib/Pinto/Config.pm
@@ -5,6 +5,7 @@ package Pinto::Config;
 use Moose;
 
 use MooseX::Configuration;
+use MooseX::Aliases;
 
 use MooseX::Types::Moose qw(Str Bool Int);
 use Pinto::Types qw(Dir File);
@@ -22,19 +23,10 @@ use namespace::autoclean;
 has root       => (
     is         => 'ro',
     isa        => Dir,
+    alias      => 'root_dir',
     required   => 1,
     coerce     => 1,
 );
-
-
-has root_dir   => (            # An alias for 'root'
-    is         => 'ro',
-    isa        => Dir,
-    init_arg   => undef,
-    default    => sub { return $_[0]->root() },
-    lazy       => 1,
-);
-
 
 has authors_dir => (
     is        => 'ro',
