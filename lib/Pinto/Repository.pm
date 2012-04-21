489d5d04a581ef2dd0f17fdc06b138fcd0680ad7
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-04-21 01:22:31 -0700

    Rewrite the entire Repository interface.

--- lib/Pinto/Repository.pm
+++ lib/Pinto/Repository.pm
@@ -1,6 +1,6 @@
-package Pinto::Repository;
-
 # ABSTRACT: Coordinates the database, files, and indexes
+
+package Pinto::Repository;
 
 use Moose;
 
@@ -27,15 +27,31 @@ with qw( Pinto::Role::Configurable
 
 #-------------------------------------------------------------------------------
 
+=attr db
+
+=method write_index
+
+=cut
+
 has db => (
     is         => 'ro',
     isa        => 'Pinto::Database',
     lazy       => 1,
-    handles    => [ qw(write_index select_distributions select_packages) ],
+    handles    => [ qw(write_index) ],
     default    => sub { Pinto::Database->new( config => $_[0]->config,
                                               logger => $_[0]->logger ) },
 );
 
+
+=attr store
+
+=method initialize()
+
+=method commit()
+
+=method tag()
+
+=cut
 
 has store => (
     is         => 'ro',
@@ -48,15 +64,28 @@ has store => (
                                            logger => $_[0]->logger ) },
 );
 
+=attr cache
+
+=method locate( package => );
+
+=method locate( distribution => );
+
+=cut
 
 has cache => (
     is         => 'ro',
     isa        => 'Pinto::IndexCache',
     lazy       => 1,
+    does       => [ qw(locate) ],
     default    => sub { Pinto::IndexCache->new( config => $_[0]->config,
                                                 logger => $_[0]->logger ) },
 );
 
+=method lock
+
+=method unlock
+
+=cut
 
 has locker  => (
     is         => 'ro',
@@ -67,6 +96,9 @@ has locker  => (
                                             logger => $_[0]->logger ) },
 );
 
+=attr extractor
+
+=cut
 
 has extractor => (
     is         => 'ro',
@@ -87,24 +119,59 @@ has revision => (
 
 #-------------------------------------------------------------------------------
 
-=method get_latest_package( name => $package_name )
+=method get_stack( name => $stack_name )
 
-Returns the latest version of L<Pinto:Schema::Result::Package> with
-the given C<$package_name>.  If there is no such package with that
-name in the repository, returns C<undef>.
+Returns the L<Pinto::Schema::Result::Stack> object with the given
+C<$stack_name>.  If there is no stack with such a name in the
+repository, returns C<undef>.
 
 =cut
 
-sub get_latest_package {
+sub get_stack {
+    my ($self, %args) = @_;
+
+    my $stk_name = $args{name};
+
+    my $where = { name => $stk_name };
+    my $stack = $self->db->select_stacks( $where )->single;
+
+    return $stack;
+}
+
+#-------------------------------------------------------------------------------
+
+=method get_package( name => $pkg_name )
+
+Returns the latest version of L<Pinto:Schema::Result::Package> with
+the given C<$pkg_name>.  If there is no such package with that name in the
+repository, returns nothing.
+
+=method get_package( name => $pkg_name, stack => $stk_name )
+
+Returns the L<Pinto:Schema::Result::Package> with the given
+C<$pkg_name> that is on the stack with the given C<$stk_name>. If
+there is no such package on that stack, returns nothing.
+
+=cut
+
+sub get_package {
     my ($self, %args) = @_;
 
     my $pkg_name = $args{name};
+    my $stk_name = $args{stack};
 
-    my $where = { name => $pkg_name };
-    my @packages = $self->db->select_packages( $where )->all();
-    my $latest   = (sort {$a <=> $b} @packages)[-1];
-
-    return $latest;
+    if ($stk_name) {
+        my $attrs = { prefetch => [ qw(package stack) ] };
+        my $where = { 'package.name' => $pkg_name, 'stack.name' => $stk_name };
+        my $pkg_stk = $self->db->select_package_stacks($where, $attrs)->single;
+        return $pkg_stk->package;
+    }
+    else {
+        my $where  = { name => $pkg_name };
+        my @pkgs   = $self->db->select_packages( $where )->all;
+        my $latest = (sort {$a <=> $b} @pkgs)[-1];
+        return $latest;
+    }
 }
 
 #-------------------------------------------------------------------------------
@@ -113,7 +180,7 @@ sub get_latest_package {
 
 Returns the L<Pinto::Schema::Result::Distribution> with the given
 C<$dist_path>.  If there is no distribution with such a path in the
-respoistory, returns C<undef>.  Note the C<$dist_path> is a Unix-style
+respoistory, returns nothing.  Note the C<$dist_path> is a Unix-style
 path fragment that identifies the location of the distribution archive
 within the repository, such as F<J/JE/JEFF/Pinto-0.033.tar.gz>
 
@@ -126,61 +193,27 @@ sub get_distribution {
 
     my $where = { path => $dist_path };
     my $attrs = { prefetch => 'packages' };
-    my $dist  = $self->db->select_distributions( $where, $attrs )->first();
+    my $dist  = $self->db->select_distributions( $where, $attrs )->first;
 
     return $dist;
 }
 
 #-------------------------------------------------------------------------------
 
-=method get_stack( name => $stack_name )
+=method add( archive => $path, author => $id )
 
-Returns the L<Pinto::Schema::Result::Stack> object with the given
-C<$stack_name>.  If there is no stack with such a name in the
-repository, returns C<undef>.
+=method add( archive => $path, author => $id, source => $url )
 
-=cut
-
-sub get_stack {
-   my ($self, %args) = @_;
-
-    my $stack_name = $args{name};
-
-    my $where = { name => $stack_name };
-    my $stack  = $self->db->select_stacks( $where )->single();
-
-    return $stack;
-}
-
-#-------------------------------------------------------------------------------
-
-=method get_stack_member( package => $package_name, stack => $stack_name )
-
-Returns the L<Pinto::Schema::Result::PackageStack> object with the
-given C<$package_name> and C<$stack_name>.  If there is no
-PackageStack with such names in the repository, returns C<undef>.  A
-PackageStack represents the relationship between a Package and a
-Stack.
+Adds the distribution archive located on the local filesystem at
+C<$path> to the repository in the author directory for the author with
+C<$id>.  The packages provided by the distribution will be indexed,
+and the prerequisites will be recorded.  If the the C<source> is
+specified, it must be the URL to the root of the repository where the
+distribution came from.  Otherwise, the C<source> defaults to
+C<LOCAL>.  Returns a L<Pinto::Schema::Result::Distribution> object
+representing the newly added distribution.
 
 =cut
-
-sub get_stack_member {
-    my ($self, %args) = @_;
-
-    my $package_name = $args{package};
-    my $stack_name   = $args{stack};
-
-    my $where = { 'package.name' => $package_name,
-                  'stack.name'   => $stack_name };
-
-    my $attrs = { prefetch => [ qw(package stack) ] };
-
-    my $pkg_stk = $self->db->select_package_stacks($where, $attrs)->single();
-
-    return $pkg_stk;
-}
-
-#-------------------------------------------------------------------------------
 
 sub add {
     my ($self, %args) = @_;
@@ -188,7 +221,7 @@ sub add {
     my $archive = $args{archive};
     my $author  = $args{author};
     my $source  = $args{source} || 'LOCAL';
-    my $index   = $args{index}  || 1;
+    my $index   = $args{index}  || 1;  # Is this needed?
 
     confess "Archive $archive does not exist"  if not -e $archive;
     confess "Archive $archive is not readable" if not -r $archive;
@@ -225,13 +258,24 @@ sub add {
 }
 
 #------------------------------------------------------------------------------
-# TODO: Maybe refactor this to use the add() method
+
+=method mirror( struct => {} )
+
+Mirrors a distribution archive from another repository.  The
+C<struct> defines the attributes of the distribution and any packages
+it contains. TODO: document the members of the struct.  The pacakges
+provided by the distribution will not be indexed.  Returns a
+L<Pinto::Schema::Result::Distribution> object representing the newly
+mirrored distribution.
+
+=cut
 
 sub mirror {
     my ($self, %args) = @_;
 
     my $struct = $args{struct};
 
+    # TODO: Maybe refactor this to use the add() method
     my $url = URI->new($struct->{source} . '/authors/id/' . $struct->{path});
     $self->info("Mirroring distribution at $url");
 
@@ -240,7 +284,7 @@ sub mirror {
     $struct->{md5}    = Pinto::Util::md5($temp_archive);
     $struct->{sha256} = Pinto::Util::sha256($temp_archive);
 
-    my $dist = $self->db->create_distribution($struct, $stack);
+    my $dist = $self->db->create_distribution($struct);
 
     my @path_parts = split m{ / }mx, $struct->{path};
     my $repo_archive = $self->root_dir->file( qw(authors id), @path_parts );
@@ -252,6 +296,19 @@ sub mirror {
 
 #------------------------------------------------------------------------------
 
+=method pull( url => $url )
+
+Pulls a distribution archive from a remote repository and C<add>s it
+to this repository.  The packages provided by the distribution will be
+indexed, and the prerequisites will be recorded.  Returns a
+L<Pinto::Schema::Result::Distribution> object representing the newly
+pulled distribution.
+
+=method pull( package => $spec )
+
+=method pull( distribution => $spec )
+
+=cut
 
 sub pull {
     my ($self, %args) = @_;
@@ -272,64 +329,314 @@ sub pull {
 
 #-------------------------------------------------------------------------------
 
-sub remove_distribution {
-    my ($self, %args) = @_;
+=method register( distribution => $dist, stack => $stack )
 
-    my $dist = $args{dist};
+Registers a distribution to a stack.  All packages in the distribution
+will replace those with the same name that are already in the stack.
+Returns a true value if the contents of the stack were actually
+changed.
 
-    my $count = $dist->package_count();
-    $self->notice("Removing distribution $dist with $count packages");
-
-    $self->db->delete_distribution($dist);
-
-    $self->store->remove_archive( $dist->archive( $self->root_dir() ) );
-
-    return $dist;
-}
-
-#-------------------------------------------------------------------------------
+=cut
 
 sub register {
     my ($self, %args) = @_;
 
     my $dist  = $args{distribution};
-    my $pkg   = $args{package};
     my $stack = $args{stack};
     my $did_register = 0;
 
-    if ($dist) {
-        $self->info("Registering distribution $dist on stack $stack");
-        $did_register += $self->db->register($_, $stack) for $dist->packages;
-    }
-    elsif ($pkg) {
-        $self->info("Registering package $pkg on stack $stack");
-        $did_register += $self->db->register($pkg, $stack);
-    }
+    $self->info("Registering distribution $dist on stack $stack");
+    $did_register += $self->db->register($_, $stack) for $dist->packages;
 
     return $did_register;
 }
 
 #-------------------------------------------------------------------------------
 
+=method unregister( distribution => $dist, stack => $stack )
+
+Unregisters a distribution from a stack.  All packages in the
+distribution will be removed from the stack.  Returns a true value if
+the contents of the stack were actually changed.
+
+=cut
+
+sub unregister {
+    my ($self, %args) = @_;
+
+    my $dist  = $args{distribution};
+    my $stack = $args{stack};
+    my $did_unregister = 0;
+
+    $self->info("Unregistering distribution $dist from stack $stack");
+    $did_unregister += $self->db->unregister($_, $stack) for $dist->packages;
+
+    return $did_unregister;
+}
+
+#-------------------------------------------------------------------------------
+
+=method pin( distribution => $dist, stack => $stack )
+
+Pins all the packages in the distribution to the stack.  These
+packages cannot be displaced until you unpin them.  Returns a true
+value if any packages were actually pinned.
+
+=cut
+
 sub pin {
     my ($self, %args) = @_;
 
-    my $dist   = $args{distribution};
-    my $pkg    = $args{package};
-    my $stack  = $args{stack};
+    my $dist    = $args{distribution};
+    my $stack   = $args{stack};
+    my $did_pin = 0;
 
-    if ($dist) {
-        $self->info("Pinning distribution $dist on stack $stack");
-        $self->db->pin($_, $stack) for $dist->packages;
-    }
-    elsif ($pkg) {
-        $self->info("Pinning package $pkg on stack $stack");
-        $self->db->pin($pkg, $stack);
-    }
+    $self->notice("Pinning distribution $dist on stack $stack");
+    $did_pin += $self->db->pin($_, $stack) for $dist->packages;
 
-    return $self;
+    return $did_pin;
 
 }
+
+#-------------------------------------------------------------------------------
+
+=method unpin( distribution => $dist, stack => $stack )
+
+Unpins all the packages in the distribution from the stack.  These
+packages can be displaced.  Returns a true value if any packages were
+actually unpinned.
+
+=cut
+
+sub unpin {
+    my ($self, %args) = @_;
+
+    my $dist   = $args{distribution};
+    my $stack  = $args{stack};
+    my $did_unpin = 0;
+
+    $self->notice("Unpinning distribution $dist from stack $stack");
+    $did_unpin += $self->db->unpin($_, $stack) for $dist->packages;
+
+    return $did_unpin;
+}
+
+#-------------------------------------------------------------------------------
+
+=method create_stack(name => $stk_name, description => $why)
+
+=cut
+
+sub create_stack {
+    my ($self, %args) = @_;
+
+    $self->info("Creating stack $args{name}");
+
+    my $stack = $self->db->create_stack( \%args );
+
+    return $stack;
+
+}
+
+#-------------------------------------------------------------------------------
+
+=method remove_stack(name => $stk_name)
+
+=cut
+
+sub remove_stack {
+    my ($self, %args) = @_;
+
+    my $stk_name = $args{name};
+
+    $self->fatal( 'You cannot remove the default stack' )
+        if $stk_name eq 'default';
+
+    $self->info("Removing stack $stk_name");
+
+    my $stack = $self->get_stack( name => $stk_name );
+
+    $stack->delete;
+
+    return;
+
+}
+
+#-------------------------------------------------------------------------------
+
+=method copy_stack(from => $stack_a, to => $stk_name_b)
+
+=method copy_stack(from => $stack_a, to => $stk_name_b, description => $why)
+
+=cut
+
+sub copy_stack {
+    my ($self, %args) = @_;
+
+    my $from_stk_name = $args{from};
+    my $to_stk_name   = $args{to};
+    my $description   = $args{description};
+
+    $self->info("Creating new stack $to_stk_name");
+
+    my $changes = { name => $to_stk_name };
+    $changes->{description} = $description if $description;
+
+    my $from_stack = $self->get_stack(name => $from_stk_name);
+    my $to_stack   = $from_stack->copy( $changes );
+
+    $self->info("Copying stack $from_stk_name into stack $to_stk_name");
+
+    for my $packages_stack ( $from_stack->packages_stack ) {
+        my $pkg = $packages_stack->package;
+        $self->debug("Copying package $pkg into stack $to_stk_name");
+        $packages_stack->copy( { stack => $to_stack->id } );
+    }
+
+    return $to_stack;
+}
+
+#-------------------------------------------------------------------------------
+
+=method merge_stack(from => $stk_name_a, to => $stk_name_b)
+
+=method merge_stack(from => $stk_name_a, to => $stk_name_b, dryrun => 1)
+
+=cut
+
+sub merge_stack {
+    my ($self, %args) = @_;
+
+    my $from_stk_name = $args{from};
+    my $to_stk_name   = $args{to};
+    my $dryrun        = $args{dryrun};
+
+
+    my $from_stk = $self->repos->get_stack(name => $from_stk_name)
+        or confess "Stack $from_stk_name does not exist";
+
+    my $to_stk = $self->repos->get_stack(name => $to_stk_name)
+        or confess "Stack $to_stk_name does not exist";
+
+    my $conflicts;
+    my $where = { stack => $from_stk->id };
+    my $pkg_stk_rs = $self->repos->db->select_package_stacks( $where );
+
+
+    while ( my $from_pkg_stk = $pkg_stk_rs->next ) {
+        $self->info("Merging package $from_pkg_stk into stack $to_stk");
+        $conflicts += $self->_merge_pkg_stk( $from_pkg_stk, $to_stk, $dryrun );
+    }
+
+
+    $self->fatal("There were $conflicts conflicts.  Merge aborted")
+        if $conflicts and not $dryrun;
+
+    $self->info('Dry run merge -- no changes were made')
+        and return if $dryrun;
+
+    # TODO: Add the is_merged field to schema
+    # $from_stk->update( {is_merged => 1} );
+
+    return;
+}
+
+#------------------------------------------------------------------------------
+
+sub _merge_pkg_stk {
+    my ($self, $from_pkg_stk, $to_stk, $dryrun) = @_;
+
+    my $from_pkg   = $from_pkg_stk->package;
+    my $attrs      = { prefetch => 'package' };
+    my $where      = { 'package.name' => $from_pkg->name,
+                       'stack'        => $to_stk->id };
+
+    my $to_pkg_stk = $self->db->select_package_stacks($where, $attrs)->single;
+
+    # CASE 1:  The package does not exist in the target stack,
+    # so we can go ahead and just add it there.
+
+    if (not defined $to_pkg_stk) {
+         $self->debug("Adding package $from_pkg to stack $to_stk");
+         return 0 if $dryrun;
+         $from_pkg_stk->copy( {stack => $to_stk} );
+         return 0;
+     }
+
+    # CASE 2:  The exact same package is in both the source
+    # and the target stacks, so we don't have to merge.  But
+    # if the source is pinned, then we should also copy the
+    # pin to the target.
+
+    if ($from_pkg_stk == $to_pkg_stk) {
+        $self->debug("$from_pkg_stk and $to_pkg_stk are the same");
+        if ($from_pkg_stk->is_pinned and not $to_pkg_stk->is_pinned) {
+            $self->debug("Adding pin to $to_pkg_stk");
+            return 0 if $dryrun;
+            $to_pkg_stk->update({pin => 1});
+            return 0;
+        }
+        return 0;
+    }
+
+    # CASE 3:  The package in the target stack is newer than the
+    # one in the source stack.  If the package in the source stack
+    # is pinned, then we have a conflict, so whine.  If it is not
+    # pinned then there is nothing to do because the package in
+    # the target stack is already newer.
+
+    if ($to_pkg_stk > $from_pkg_stk) {
+        if ( $from_pkg_stk->is_pinned ) {
+            $self->warning("$from_pkg_stk is pinned to a version older than $to_pkg_stk");
+            return 1;
+        }
+        $self->debug("$to_pkg_stk is already newer than $from_pkg_stk");
+        return 0;
+    }
+
+
+    # CASE 4:  The package in the target stack is older than the
+    # one in the source stack.  If the package in the target stack
+    # is pinned, then we have a conflict, so whine.  If it is not
+    # pinned, then upgrade the package in the target stack with
+    # the newer package in the source stack.
+
+    if ($to_pkg_stk < $from_pkg_stk) {
+        if ( $to_pkg_stk->is_pinned ) {
+            $self->warning("$to_pkg_stk is pinned to a version older than $from_pkg_stk");
+            return 1;
+        }
+        my $from_pkg = $from_pkg_stk->package();
+        $self->info("Upgrading $to_pkg_stk to $from_pkg");
+        return 0 if $dryrun;
+        $to_pkg_stk->update( {package => $from_pkg} );
+        return 0;
+    }
+
+    # CASE 5:  The above logic should cover all possible scenarios.
+    # So if we get here then either our logic is flawed or something
+    # weird has happened in the database.
+
+    confess "Unable to merge $from_pkg_stk into $to_pkg_stk";
+}
+
+#-------------------------------------------------------------------------------
+
+=method locate(path = $dist_path)
+
+=method locate(package => $name)
+
+=method locate(package => $name, version => $vers)
+
+
+=method get_or_locate(path = $dist_path)
+
+=method get_or_locate(package => $name)
+
+=method get_or_locate(package => $name, version => $vers)
+
+=cut
+
 #-------------------------------------------------------------------------------
 
 sub open_revision {
