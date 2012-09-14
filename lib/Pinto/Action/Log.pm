d7b1a2e9cd356d72e4c983db8baf4d88ac630589
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-09-14 11:13:16 -0700

    New Action for showing revision history.

---
+++ lib/Pinto/Action/Log.pm
@@ -0,0 +1,75 @@
+# ABSTRACT: Show revision log for a stack
+
+package Pinto::Action::Log;
+
+use Moose;
+use MooseX::Types::Moose qw(Bool Int Undef);
+
+use Pinto::Types qw(StackName StackDefault);
+
+use namespace::autoclean;
+
+#------------------------------------------------------------------------------
+
+# VERSION
+
+#------------------------------------------------------------------------------
+
+extends qw( Pinto::Action );
+
+#------------------------------------------------------------------------------
+
+has stack => (
+    is        => 'ro',
+    isa       => StackName | StackDefault,
+    default   => undef,
+    coerce    => 1,
+);
+
+
+has revision => (
+    is        => 'ro',
+    isa       => Int | Undef,
+    default   => undef,
+);
+
+
+has detailed => (
+    is        => 'ro',
+    isa       => Bool,
+    default   => 0,
+);
+
+#------------------------------------------------------------------------------
+
+sub execute {
+    my ($self) = @_;
+
+    my $stack = $self->repos->get_stack(name => $self->stack);
+    my $format = "r%b | %j | %u\n\n%g\n\n";
+
+    my $wanted_revision = $self->revision;
+    my @revisions = defined $wanted_revision ? $stack->revision(number => $wanted_revision)
+                                             : $stack->revisions; # plural!
+
+    $self->fatal("No such revision $wanted_revision on stack $stack")
+      if (!@revisions && defined $wanted_revision);
+
+    for my $revision (@revisions) {
+        $self->say('-' x 79);
+        $self->say($revision->to_string($format));
+        $self->say(join "\n", $revision->registration_histories) if $self->detailed;
+    }
+
+    return $self->result;
+}
+
+#------------------------------------------------------------------------------
+
+__PACKAGE__->meta->make_immutable;
+
+#------------------------------------------------------------------------------
+
+1;
+
+__END__
