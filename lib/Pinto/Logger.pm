947fc6fb45bb007e188d9b927b2e55c8380552fc
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-09-19 14:57:29 -0700

    Remove username from log messages.
    Things are simpler if we don't have to pass the
    username to the logger each time an Action is run.
    Besides, the commit log already has a record of
    who did what.

--- lib/Pinto/Logger.pm
+++ lib/Pinto/Logger.pm
@@ -50,13 +50,6 @@ has log_handler => (
     lazy     => 1,
 );
 
-
-has username => (
-    is       => 'rw',
-    isa      => Str,
-    default  => $ENV{USER} || $ENV{LOGIN} || $ENV{LOGNAME},
-);
-
 #-----------------------------------------------------------------------------
 
 sub _build_log_handler {
@@ -71,8 +64,7 @@ sub _build_log_handler {
                    my $msg   = $args{message};
                    my $level = uc $args{level};
                    my $now   = DateTime->now->iso8601;
-                   my $user  = $self->username;
-                   return "$now $user $level: $msg" };
+                   return "$now $level: $msg" };
 
 
     my $out = Log::Dispatch::File->new( min_level   => $self->log_level,
--- t/07-logger.t
+++ t/07-logger.t
@@ -16,7 +16,7 @@ use Pinto::Logger;
 {
     my $temp     = File::Temp->newdir();
     my $root     = dir($temp->dirname);
-    my $logger   = Pinto::Logger->new(root => $root, username => 'foo');
+    my $logger   = Pinto::Logger->new(root => $root);
     my $log_file = $root->subdir( qw(.pinto log) )->file('pinto.log');
 
     $logger->error('error');     # Logged
@@ -30,7 +30,7 @@ use Pinto::Logger;
     my @lines = $log_file->slurp;
     is scalar @lines, 3, 'log file contains correct number of messages';
 
-    like $lines[-1], qr/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2} foo NOTICE: notice$/,
+    like $lines[-1], qr/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2} NOTICE: notice$/,
         'logged message is correct';
 }
 
