a1b0a39b678ef00eacb69025e33b24c801f55d6c
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-11-17 01:42:35 -0800

    New Default action for marking the default stack.

---
+++ lib/Pinto/Action/Default.pm
@@ -0,0 +1,48 @@
+# ABSTRACT: Set the default stack
+
+package Pinto::Action::Default;
+
+use Moose;
+
+use Pinto::Types qw(StackName StackObject);
+
+use namespace::autoclean;
+
+#------------------------------------------------------------------------------
+
+# VERSION
+
+#------------------------------------------------------------------------------
+
+extends qw( Pinto::Action );
+
+#------------------------------------------------------------------------------
+
+has stack => (
+    is       => 'ro',
+    isa      => StackName | StackObject,
+    default  => undef,
+);
+
+#------------------------------------------------------------------------------
+
+sub execute {
+    my ($self) = @_;
+
+    my $stack    = $self->repo->get_stack($self->stack);
+    my $did_mark = $stack->mark_as_default;
+
+    $self->result->changed if $did_mark;
+
+    return $self->result;
+}
+
+#------------------------------------------------------------------------------
+
+__PACKAGE__->meta->make_immutable;
+
+#------------------------------------------------------------------------------
+
+1;
+
+__END__
