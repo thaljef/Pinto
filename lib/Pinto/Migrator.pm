a23cb65b9233c037c4a2ee5012108a23889516a5
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-12-18 15:03:52 -0800

    Basic version checking before migrating.

--- lib/Pinto/Migrator.pm
+++ lib/Pinto/Migrator.pm
@@ -4,6 +4,7 @@ package Pinto::Migrator;
 
 use Moose;
 
+use Pinto;
 use Pinto::Exception qw(throw);
 
 use namespace::autoclean;
@@ -23,7 +24,21 @@ with qw( Pinto::Role::Configurable
 sub migrate {
     my ($self) = @_;
 
-    throw 'Migration is not implemented yet';
+    my $pinto = Pinto->new(root => $self->config->root);
+
+    my $db_version = $pinto->repo->db->get_version;
+    my $schema_version = $pinto->repo->db->schema->schema_version;
+
+    die "This repository is too old to migrate\n"
+      if not defined $db_version;
+
+    die "This repository is up to date\n"
+      if $db_version == $schema_version;
+
+    die "This repository too new.  Upgrade Pinto instead\n"
+      if $db_version > $schema_version;
+
+    die "Migration is not implemented yet\n";
 }
 
 #------------------------------------------------------------------------------
