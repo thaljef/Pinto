fa47b5f26e58b29e342e62ee99eadfc4c5c13de8
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2013-02-01 20:33:25 -0800

    Added RS subclass, for common package-related queries.

---
+++ lib/Pinto/Schema/ResultSet/Package.pm
@@ -0,0 +1,24 @@
+use utf8;
+package Pinto::Schema::ResultSet::Package;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::ResultSet';
+
+#------------------------------------------------------------------------------
+
+# VERSION
+
+#------------------------------------------------------------------------------
+
+sub with_distribution {
+  my ($self, $where) = @_;
+
+  return $self->search($where || {}, {prefetch => 'distribution'});
+}
+
+#------------------------------------------------------------------------------
+1;
+
+__END__
