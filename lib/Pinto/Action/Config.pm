ffd7d4d32a08d214f787760b181ff24d259b0f6f
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2013-03-11 10:51:40 -0700

    Restore the Config action.

---
+++ lib/Pinto/Action/Config.pm
@@ -0,0 +1,98 @@
+# ABSTRACT: Show or change configuration
+
+package Pinto::Action::Config;
+
+use Moose;
+use MooseX::MarkAsMethods (autoclean => 1);
+use MooseX::Types::Moose qw(Str HashRef);
+
+use String::Format qw(stringf);
+
+use Pinto::Util qw(is_system_prop);
+use Pinto::Types qw(StackName StackDefault StackObject);
+
+#------------------------------------------------------------------------------
+
+# VERSION
+
+#------------------------------------------------------------------------------
+
+extends qw( Pinto::Action );
+
+#------------------------------------------------------------------------------
+
+with qw( Pinto::Role::Transactional
+         Pinto::Role::Colorable );
+
+#------------------------------------------------------------------------------
+
+has stack => (
+    is        => 'ro',
+    isa       => StackName | StackDefault | StackObject,
+);
+
+
+has properties => (
+    is        => 'ro',
+    isa       => HashRef,
+    predicate => 'has_properties',
+);
+
+
+has format => (
+    is      => 'ro',
+    isa     => Str,
+    default => "%p = %v",
+);
+
+#------------------------------------------------------------------------------
+
+sub execute {
+    my ($self) = @_;
+
+    my $stack = $self->repo->get_stack($self->stack);
+
+    $self->has_properties ? $self->_set_properties($stack)
+                          : $self->_show_properties($stack);
+
+    return $self->result;
+}
+
+#------------------------------------------------------------------------------
+
+sub _set_properties {
+    my ($self, $target) = @_;
+
+    $target->set_properties($self->properties);
+    $self->result->changed;
+
+    return;
+}
+
+#------------------------------------------------------------------------------
+
+sub _show_properties {
+    my ($self, $target) = @_;
+
+    my $props = $target->get_properties;
+    while ( my ($prop, $value) = each %{$props} ) {
+
+        my $string = stringf($self->format, {p => $prop, v => $value});
+        my $color  = is_system_prop($prop) ? $self->color_3 : undef; 
+        $string = $self->colorize_with_color($string, $color);
+
+        $self->say($string);
+    }
+
+    return;
+}
+
+#------------------------------------------------------------------------------
+
+__PACKAGE__->meta->make_immutable;
+
+#------------------------------------------------------------------------------
+
+1;
+
+__END__
