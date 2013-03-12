6e813ae2d2567faca645e2715d8c0ce8b60f59d2
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2013-03-12 09:53:03 -0700

    Redesigned the Differences.

--- lib/Pinto/Difference.pm
+++ lib/Pinto/Difference.pm
@@ -3,9 +3,10 @@
 package Pinto::Difference;
 
 use Moose;
+use MooseX::Types::Moose qw(ArrayRef);
 use MooseX::MarkAsMethods (autoclean => 1);
 
-use Array::Diff;
+use Pinto::Util qw(itis);
 
 use overload ( q{""} => 'to_string' );
 
@@ -17,39 +18,110 @@ use overload ( q{""} => 'to_string' );
 
 has left => (
     is       => 'ro',
-    isa      => 'Pinto::Schema::Result::Revision',
+    isa      => 'Pinto::Schema::ResultSet::Registration',
     required => 1,
 );
 
 
 has right => (
     is       => 'ro',
-    isa      => 'Pinto::Schema::Result::Revision',
+    isa      => 'Pinto::Schema::ResultSet::Registration',
     required => 1,
 );
 
 
-has _diff => (
-    init_arg  => undef,
-    handles   => [ qw(added deleted) ],
-    builder   => '_build_diff',
-    lazy      => 1,
+has diffs => (
+    is       => 'ro',
+    isa      => ArrayRef,
+    builder  => '_build_diffs',
+    init_arg => undef,
+    lazy     => 1,
 );
 
 
+has adds => (
+    is       => 'ro',
+    isa      => ArrayRef['Pinto::Schema::Result::Registration'],
+    default  => sub { map { $_->[1] } grep {$_->[0] eq '+'} @{ $_[0]->diffs } }, 
+    init_arg => undef,
+    lazy     => 1,
+);
+
+
+has dels => (
+    is       => 'ro',
+    isa      => ArrayRef['Pinto::Schema::Result::Registration'],
+    default  => sub { map { $_->[1] } grep {$_->[0] eq '-'} @{ $_[0]->diffs } }, 
+    init_arg => undef,
+    lazy     => 1,
+);
+
 #------------------------------------------------------------------------------
 
-with qw( Pinto::Role::Colorable );
+around BUILDARGS => sub {
+    my $orig  = shift;
+    my $class = shift;
+    my $args  = $class->$orig(@_);
+
+    for my $side ( qw(left right) ) {
+        my $arg = $args->{$side};
+        if (itis($arg, 'Pinto::Schema::Result::Revision')) {
+            $args->{$side} = $arg->registrations->with_package->with_distribution;
+        }
+        elsif (itis($arg, 'Pinto::Schema::Result::Stack')) {
+            $args->{$side} = $arg->head->registrations->with_package->with_distribution;
+        }
+    }
+
+    return $args;
+};
 
 #------------------------------------------------------------------------------
 
-sub _build_diff {
+sub _build_diffs {
     my ($self) = @_;
 
-    my @left  = sort {$a cmp $b } $self->left->registrations;
-    my @right = sort {$a cmp $b } $self->right->registrations;
+    my $cb = sub {
+        my ($self) = @_;
+        my @fields = qw(distribution package is_pinned);
+        return join '|', map {$self->get_column($_)} @fields;
+    };
 
-    return Array::Diff->diff(\@left, \@right);
+    # Compute left and right sets as hashes
+    my %left  = $self->left->as_hash($cb);
+    my %right = $self->right->as_hash($cb);
+
+    # Compute differences between left and right sets
+    my @added   = @right{ grep { not exists $left{$_}  } keys %right };
+    my @deleted = @left{  grep { not exists $right{$_} } keys %left  };
+
+    # Construct an ordered list of differences
+    my @adds = map { ['+' => $_] } @added;
+    my @dels = map { ['-' => $_] } @deleted;
+
+    # Strictly speaking, the registrations are an unordered list.  But
+    # the diff is more readable if we group related registrations together.
+    # So we sort them by distribution and package name.
+
+    my @diffs = sort {    
+        ($a->[1]->distribution  cmp $b->[1]->distribution) || 
+        ($a->[1]->package_name  cmp $b->[1]->package_name)        
+    } @adds, @dels;
+
+    return \@diffs;
+}
+
+#------------------------------------------------------------------------------
+
+sub foreach {
+    my ($self, $cb) = @_;
+
+    for my $diff ( @{ $self->diffs } ){
+        my ($op, $reg) = @{$diff};
+        $cb->($op, $reg); 
+    }
+
+    return $self;
 }
 
 #------------------------------------------------------------------------------
@@ -59,20 +131,8 @@ sub to_string {
 
     my $string = '';
     my $format = "%m%s%y %-40p %12v %a/%f\n";
-
-    for my $added (@{ $self->added }) {
-        my $line = '+' . $added->to_string($format);
-        my $color  = $self->color_1;
-        $string .= $self->colorize_with_color($line, $color);
-    }
-
-    for my $deleted (@{ $self->deleted }) {
-        my $line = '-' . $deleted->to_string($format);
-        my $color  = $self->color_3;
-        $string .= $self->colorize_with_color($line, $color);
-    }
-
-    # TODO: Group output lines into a sensible order
+    my $cb = sub { $string .= $_[0] . $_[1]->to_string($format)};
+    $self->foreach($cb);
 
     return $string;
 }
---
+++ lib/Pinto/Schema/ResultSet/Registration.pm
@@ -0,0 +1,54 @@
+# ABSTRACT: Common queries for Registrations
+
+use utf8;
+package Pinto::Schema::ResultSet::Registration;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::ResultSet';
+
+#------------------------------------------------------------------------------
+
+# VERSION
+
+#------------------------------------------------------------------------------
+
+sub with_package {
+  my ($self, $where) = @_;
+
+  return $self->search($where || {}, {prefetch => 'package'});
+}
+
+#------------------------------------------------------------------------------
+
+sub with_distribution {
+  my ($self, $where) = @_;
+
+  return $self->search($where || {}, {prefetch => 'distribution'});
+}
+
+#------------------------------------------------------------------------------
+
+sub with_revision {
+  my ($self, $where) = @_;
+
+  return $self->search($where || {}, {revision => 'distribution'});
+}
+
+#------------------------------------------------------------------------------
+
+sub as_hash {
+	my ($self, $cb) = @_;
+
+	$cb ||= sub {return $_[0]->id};
+	my %hash = map { ($cb->($_) => $_)  } $self->all;
+
+    return wantarray ? %hash : \%hash;
+}
+
+
+#------------------------------------------------------------------------------
+1;
+
+__END__
