60d9c5d8fae02b5a57a0307b9bba93b2ad4158a0
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2013-03-20 16:14:33 -0700

    POD edit.

--- lib/Pinto.pm
+++ lib/Pinto.pm
@@ -136,14 +136,13 @@ See L<Pinto::Manual> for more information about the Pinto tools.
 
 =head1 DESCRIPTION
 
-Pinto is a suite of tools and libraries for creating and managing a
-custom CPAN-like repository of Perl modules.  The purpose of such a
-repository is to provide a stable, curated stack of dependencies from
-which you can reliably build, test, and deploy your application using
-the standard Perl tool chain. Pinto supports various operations for
-gathering and managing distribution dependencies within the
-repository, so that you can control precisely which dependencies go
-into your application.
+Pinto is an application for creating and managing a custom CPAN-like 
+repository of Perl modules.  The purpose of such a repository is to 
+provide a stable, curated stack of dependencies from which you can 
+reliably build, test, and deploy your application using the standard 
+Perl tool chain. Pinto supports various operations for gathering and 
+managing distribution dependencies within the repository, so that you 
+can control precisely which dependencies go into your application.
 
 =head1 FEATURES
 
