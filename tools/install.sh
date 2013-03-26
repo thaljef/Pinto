9804cf80f3165ab908468f6c57e5e3e86a6f8b0d
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2013-03-26 15:55:02 -0700

    Make cpanm quiet.  Die if it fails.

--- tools/install.sh
+++ tools/install.sh
@@ -3,8 +3,10 @@
 export PINTO_HOME=$HOME/usr/local/pinto
 export PATH=$PINTO_HOME/bin:$PATH
 
+echo "Installing pinto into $PINTO_HOME"
 PINTO_REPO_URL=http://stratopan.com/Stratopan/Pinto/Production
-cpanm --mirror $PINTO_REPO_URL --mirror-only  --local-lib-contained $PINTO_HOME App::Pinto Pinto
+cpanm --notest --quiet --mirror $PINTO_REPO_URL --mirror-only  --local-lib-contained $PINTO_HOME App::Pinto Pinto
+if [ $? -ne 0 ] ; then echo "Installation failed.  See the cpanm build log for details"; exit 1; fi
 
 echo <<END_MSG
 pinto has been installed at $PINTO_HOME
@@ -13,6 +15,9 @@ Now add the following to your ~/.profile
 
   export PINTO_HOME=$PINTO_HOME
   export PATH=\$PINTO_HOME/:\$PATH
+
+Thank you for trying pinto.  
+Send feeback to thaljef@cpan.org
 END_MSG
 
 exit 0;
