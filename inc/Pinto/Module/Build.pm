a735bfad70ea15c3b4053a709f133ee425d68f1f
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2013-04-03 13:26:25 -0700

    Use M::B::CleanInstall to remove old files when installing.

---
+++ inc/Pinto/Module/Build.pm
@@ -0,0 +1,11 @@
+package Pinto::Module::Build;
+
+use strict;
+use warnings;
+
+use base 'Module::Build::CleanInstall';
+
+#------------------------------------------------------------------------------
+1;
+
+__END__
