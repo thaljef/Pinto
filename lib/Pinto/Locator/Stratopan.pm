9f128811714cea5dc1f71af221353b0a139ae85c
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2014-01-16 15:02:39 -0800

    First semi-working draft of the Stratopan locator service.

--- lib/Pinto/Locator/Stratopan.pm
+++ lib/Pinto/Locator/Stratopan.pm
@@ -6,9 +6,12 @@ use Moose;
 use MooseX::StrictConstructor;
 use MooseX::MarkAsMethods ( autoclean => 1 );
 
+use URI;
 use JSON qw(decode_json);
 use URI::Escape qw(uri_escape);
 use HTTP::Request::Common qw(GET);
+
+use Pinto::Util qw(whine);
 
 #-----------------------------------------------------------------------------
 
@@ -28,9 +31,21 @@ sub locate_package {
 	my ($self, %args) = @_;
 
 	my $target = $args{target};
-	my $url = $self->build_query_url("$target")
-	my $res = $self->request(GET(url));
-	my $struct = decode_json($res->content);
+	my $url = $self->build_query_url("$target");
+	my $res = $self->request(GET($url));
+
+	if (!$res->is_success) {
+		whine "Stratopan is not responding: " . $res->status_line;
+		return;
+	}
+
+	my $structs = decode_json($res->content);
+	return unless my $latest = $structs->[0];
+
+	$latest->{version} = version->parse($latest->{version});
+	$latest->{url} = URI->new($latest->{url});
+
+	return $latest;
 }
 
 #-----------------------------------------------------------------------------
@@ -39,10 +54,21 @@ sub locate_distribution {
 	my ($self, %args) = @_;
 
 	my $target = $args{target};
-	my $url = $self->build_query_url("$target")
-	my $res = $self->request(GET(url));
-	my $struct = decode_json($res->content);
+	my $url = $self->build_query_url("$target");
+	my $res = $self->request(GET($url));
 
+	if (!$res->is_success) {
+		whine "Stratopan is not responding: " . $res->status_line;
+		return;
+	}
+
+	my $structs = decode_json($res->content);
+	return unless my $latest = $structs->[0];
+
+	$latest->{version} = version->parse($latest->{version});
+	$latest->{url} = URI->new($latest->{url});
+
+	return $latest;
 }
 
 #-----------------------------------------------------------------------------
@@ -50,7 +76,7 @@ sub locate_distribution {
 sub build_query_url {
 	my ($self, $query) = @_;
 
-	return sprintf "%s&q=%s", $stratopan_base_url, uri_escape($target);
+	return sprintf "%s?q=%s", $stratopan_base_url, uri_escape($query);
 }
 
 #-----------------------------------------------------------------------------
