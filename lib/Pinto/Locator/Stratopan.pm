37add356b90dcaf974281a0e258c9479e8491772
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2014-01-16 05:02:18 -0800

    Stubbing out more of the Stratopan locator.

--- lib/Pinto/Locator/Stratopan.pm
+++ lib/Pinto/Locator/Stratopan.pm
@@ -6,6 +6,10 @@ use Moose;
 use MooseX::StrictConstructor;
 use MooseX::MarkAsMethods ( autoclean => 1 );
 
+use JSON qw(decode_json);
+use URI::Escape qw(uri_escape);
+use HTTP::Request::Common;
+
 #-----------------------------------------------------------------------------
 
 # VERSION
@@ -16,11 +20,38 @@ extends qw(Pinto::Locator);
 
 #-----------------------------------------------------------------------------
 
-sub locate_package {}
+my $stratopan_base_url = URI->new('http://meta.stratopan.com/locate');
 
 #-----------------------------------------------------------------------------
 
-sub locate_distribution {}
+sub locate_package {
+	my ($self, %args) = @_;
+
+	my $target = $args{target};
+	my $url = $self->build_query_url("$target")
+	my $res = $self->request(GET(url));
+	my $struct = decode_json($res->content);
+}
+
+#-----------------------------------------------------------------------------
+
+sub locate_distribution {
+	my ($self, %args) = @_;
+
+	my $target = $args{target};
+	my $url = $self->build_query_url("$target")
+	my $res = $self->request(GET(url));
+	my $struct = decode_json($res->content);
+	
+}
+
+#-----------------------------------------------------------------------------
+
+sub build_query_url {
+	my ($self, $query) = @_;
+
+	return sprintf "%s&q=%s", $stratopan_base_url, uri_escape($target);
+}
 
 #-----------------------------------------------------------------------------
 
