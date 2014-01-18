a79aa63c9c493ce3d1808d3259fe586e55703ca5
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2014-01-18 15:13:06 -0800

    Refactor common code.

--- lib/Pinto/Locator/Stratopan.pm
+++ lib/Pinto/Locator/Stratopan.pm
@@ -8,7 +8,6 @@ use MooseX::MarkAsMethods ( autoclean => 1 );
 
 use URI;
 use JSON qw(decode_json);
-use URI::Escape qw(uri_escape);
 use HTTP::Request::Common qw(GET);
 
 use Pinto::Util qw(whine);
@@ -27,22 +26,7 @@ extends qw(Pinto::Locator);
 sub locate_package {
 	my ($self, %args) = @_;
 
-	my $target = $args{target};
-	my $uri = $self->build_query_uri("$target");
-	my $res = $self->request(GET($uri));
-
-	if (!$res->is_success) {
-		whine "Stratopan is not responding: " . $res->status_line;
-		return;
-	}
-
-	my $structs = decode_json($res->content);
-	return unless my $latest = $structs->[0];
-
-	$latest->{version} = version->parse($latest->{version});
-	$latest->{uri} = URI->new($latest->{uri});
-
-	return $latest;
+	return $self->_locate_any(%args);
 }
 
 #-----------------------------------------------------------------------------
@@ -50,30 +34,33 @@ sub locate_package {
 sub locate_distribution {
 	my ($self, %args) = @_;
 
-	my $target = $args{target};
-	my $uri = $self->build_query_uri("$target");
-	my $res = $self->request(GET($uri));
+	return $self->_locate_any(%args);
+}
 
-	if (!$res->is_success) {
-		whine "Stratopan is not responding: " . $res->status_line;
+#-----------------------------------------------------------------------------
+
+sub _locate_any {
+	my ($self, %args) = @_;
+
+	my $uri = $PINTO_STRATOPAN_LOCATOR_URI->clone;
+	$uri->query_form(q => $args{target}->to_string);
+	my $response = $self->request(GET($uri));
+
+	if (!$response->is_success) {
+		my $status = $response->status_line;
+		whine "Stratopan is not responding: $status";
 		return;
 	}
 
-	my $structs = decode_json($res->content);
+	my $structs = eval { decode_json($response->content) };
+	whine "Invalid response from Stratopan: $@" and return if $@;
+
 	return unless my $latest = $structs->[0];
 
 	$latest->{version} = version->parse($latest->{version});
 	$latest->{uri} = URI->new($latest->{uri});
 
 	return $latest;
-}
-
-#-----------------------------------------------------------------------------
-
-sub build_query_uri {
-	my ($self, $query) = @_;
-
-	return sprintf "%s?q=%s", $PINTO_STRATOPAN_LOCATOR_URI, uri_escape($query);
 }
 
 #-----------------------------------------------------------------------------
